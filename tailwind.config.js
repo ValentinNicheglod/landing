fa17d776b5593b318ff2ac48902ab91962b99d6c
Valentín Nicheglod <nicheglod69@gmail.com>
2024-09-23 02:58:03 -0300

    Scroll Instruction Implementation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  important: true,
   content: [
     './src/**/*.{astro,html,js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-animated')
+  ],
 }
