ec6ed6d722226fa9e4a4ad06df1f48a13200a216
Valentín Nicheglod <nicheglod69@gmail.com>
2024-10-19 17:21:10 -0300

    Project Card Component - Technologies

--- src/models/constants/index.ts
+++ src/models/constants/index.ts
@@ -1,1 +1,15 @@
 export const isProduction = false;
+export const technologiesWithOwnBG = new Set([
+  "Angular",
+  "Bootstrap",
+  "CSS",
+  "GitHub",
+  "HTML",
+  "Illustrator",
+  "JS",
+  "Photoshop",
+  "Postman",
+  "Prettier",
+  "TS",
+  "XD",
+]);
