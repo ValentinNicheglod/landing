e18728812414817f1a129ed194a7bc3905b248c1
Valentín Nicheglod <nicheglod69@gmail.com>
2024-12-18 12:37:23 -0300

    Gradient Background Optimization

---
+++ src/scripts/animations.scripts.ts
@@ -0,0 +1,54 @@
+type GradientSections = "skills" | "contact";
+const SECOND = 1000;
+
+const translateGradientFigures = (sectionId: GradientSections) => {
+  const container = document.getElementById(`${sectionId}-gradient`);
+
+  if (!container) return;
+
+  const getTranslation = (axis: "x" | "y") => {
+    const TRANSLATE_MULTIPLIER = 1.25;
+    const RANDOM_OFFSET = Math.ceil(Math.random() * 10);
+    const MAX_AXIS_TRANSLATION =
+      container[axis === "x" ? "offsetWidth" : "offsetHeight"];
+    const translation =
+      (MAX_AXIS_TRANSLATION * TRANSLATE_MULTIPLIER) / RANDOM_OFFSET;
+    return (RANDOM_OFFSET > 5 ? translation : -translation) + "px";
+  };
+
+  const gradientCircles = container.getElementsByClassName("gradient-circle");
+  for (const circle of gradientCircles) {
+    setTimeout(() => {
+      circle.setAttribute(
+        "style",
+        `transform: translateX(${getTranslation("x")}) translateY(${getTranslation("y")})`,
+      );
+    }, SECOND);
+  }
+};
+
+let animationRunning = false;
+const initGradientsBackgroundAnimations = () => {
+  if (animationRunning) return;
+  animationRunning = true;
+
+  const sections: GradientSections[] = ["skills", "contact"];
+  for (const section of sections) {
+    translateGradientFigures(section);
+  }
+
+  setTimeout(() => {
+    animationRunning = false;
+    requestAnimationFrame(initGradientsBackgroundAnimations);
+  }, SECOND * 10);
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const reducedMotion =
+    window.matchMedia(`(prefers-reduced-motion: reduce)`) ||
+    window.matchMedia(`(prefers-reduced-motion: reduce)`).matches === true;
+
+  if (!!reducedMotion) return;
+
+  initGradientsBackgroundAnimations();
+});
