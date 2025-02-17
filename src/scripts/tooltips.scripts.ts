d58ce62dff9d4368b9c5e58192f9d2989cf7e1a8
Valentín Nicheglod <nicheglod69@gmail.com>
2025-02-17 12:08:39 -0300

    Social Links - External link icon added to tooltip

--- src/scripts/tooltips.scripts.ts
+++ src/scripts/tooltips.scripts.ts
@@ -126,9 +126,25 @@ export const setThemeTooltipData = (theme: "light" | "dark") => {
 export const setSocialLinksTooltips = () => {
   const buttons = ["Behance", "Dribbble", "GitHub", "LinkedIn"];
   buttons.forEach((title) => {
+    const content = `
+      <div class="flex items-center gap-2">
+        ${title}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="18" height="18" viewBox="0 0 24 24"
+          stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+          class="feather feather-external-link" fill="none"
+        >
+          <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+          <polyline points="15 3 21 3 21 9"></polyline>
+          <line x1="10" y1="14" x2="21" y2="3"></line>
+        </svg>
+      </div>
+    `;
     tippy(`.social[name="${title}"]`, {
       ...defaultConfig,
-      content: title,
+      content,
+      allowHTML: true
     });
   });
 };
