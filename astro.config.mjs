80993acb99fbdb181df0f3a4d77a9f8cf5d509c5
Valentín Nicheglod <nicheglod69@gmail.com>
2025-03-04 21:29:51 -0300

    Deploy Config Added

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://valentinnicheglod.github.io',
+  base: 'landing',
   integrations: [icon()],
   i18n: {
     defaultLocale: "en",
