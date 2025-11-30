adbd1cac81bcc8e29a5e0a02c910358ef066f7ac
Valentín Nicheglod <nicheglod69@gmail.com>
2025-11-30 11:58:57 -0300

    Site Map Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,32 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import icon from 'astro-icon';
+import icon from "astro-icon";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://valentin-nicheglod.is-a.dev',
+  site: "https://valentin-nicheglod.is-a.dev",
   build: {
-    format: 'preserve',
+    format: "preserve",
   },
-  integrations: [icon(), sitemap()],
+  integrations: [
+    icon(),
+    sitemap({
+      i18n: {
+        locales: {
+          en: "en",
+          es: "es",
+        },
+        defaultLocale: "en",
+      },
+    }),
+  ],
   i18n: {
     defaultLocale: "en",
     locales: ["es", "en"],
     routing: {
-      prefixDefaultLocale: true
-    }
-  }
+      prefixDefaultLocale: true,
+    },
+  },
 });
