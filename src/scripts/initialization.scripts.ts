8c489dd20d0560d737c024bada8079f075bb0306
Valentín Nicheglod <nicheglod69@gmail.com>
2026-03-18 18:01:09 -0300

    remove loader logic

--- src/scripts/initialization.scripts.ts
+++ src/scripts/initialization.scripts.ts
@@ -1,5 +1,3 @@
-import { isProduction } from "../models/constants";
-
 /** If user navigator language is Spanish the page will be initialized in that language instead of English */
 export const initializeLanguage = () => {
   const notLanguagePreferenceSaved = !sessionStorage.getItem("lang");
@@ -10,34 +8,4 @@ export const initializeLanguage = () => {
       sessionStorage.setItem("lang", "es");
     }
   }
-};
-
-/** Show page content when animation loading finished */
-export const showContent = () => {
-  const LOADER_DURATION = isProduction ? 3000 : 0;
-  const content = document.getElementById("content");
-  const loader = document.getElementById("loader");
-
-  if (!loader) return;
-
-  setTimeout(() => {
-    content?.classList.remove("hidden");
-    loader.remove();
-  }, LOADER_DURATION);
-};
-
-export const removeLoader = () => {
-  const wrapper = document.querySelector(".wrapper");
-  const image = document.querySelector(".logo");
-  setTimeout(() => {
-    wrapper?.classList.remove("scale-150");
-    wrapper?.classList.add("scale-0");
-    image?.classList.add(
-      "animate-jump-out",
-      "animate-duration-[2000ms]",
-      "animate-ease-out",
-      "animate-delay-500",
-      "opacity-0",
-    );
-  }, 1000);
 };
